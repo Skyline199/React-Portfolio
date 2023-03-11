c5cfac1954eddea1cd4ccf8920aad756c4bf10f9
unknown <aniljassi88@gmail.com>
2023-03-11 13:25:42 +0000

    addded navbar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// import "bootstrap/scss/bootstrap";
+import "bootstrap/package.json"
 
 function App() {
   //const styles = {
    //width:  "18px"
   //}
   return (
+    
     <div className="App">
       <div className="card" style={{width: "18rem"}}>
-        <img src="/logo192.png" className="card-img-top" alt="svg logo"></img>
+        <img src="" className="card-img-top" alt=""></img>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -15,7 +16,8 @@ function App() {
           </div>
       </div>
     </div>
-  );
+    
+  )
 }
 
 export default App;
