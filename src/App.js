9308af523c49d068ba6872a2fe58b1a3f72cb391
unknown <aniljassi88@gmail.com>
2023-03-11 12:39:08 +0000

    added bootstap cdn for card container  - package bootstrap error

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import "bootstrap/scss/bootstrap";
+
+function App() {
+  //const styles = {
+   //width:  "18px"
+  //}
+  return (
+    <div className="App">
+      <div className="card" style={{width: "18rem"}}>
+        <img src="/logo192.png" className="card-img-top" alt="svg logo"></img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="https://google.com" className="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
